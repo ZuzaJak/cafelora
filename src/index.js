2e0f8e06657fe9871b2507de6c87ab2cfff62a36
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-01 12:55:24 +0100

    ingredience jako komponenty

--- src/index.js
+++ src/index.js
@@ -24,3 +24,21 @@ orderBtn.addEventListener('click', () => {
     orderBtn.textContent = 'Objednat';
   }
 });
+// Ingredience jako komponenty
+const appElm = document.querySelector('#app');
+const Layer = (props) => {
+  const element = document.createElement('div');
+  element.className = 'layer';
+  element.innerHTML = `
+  <div class="layer__color" style="background-color: ${props.color}></div>
+  <div class="layer__label">${props.label}</div>`;
+  return element;
+};
+
+const layer1 = Layer({ color: '#feeeca', label: 'mléčná pěna' });
+const layer2 = Layer({ color: '#fed7b0', label: 'teplé mléko' });
+const layer3 = Layer({ color: '#613916', label: 'espresso' });
+
+appElm.appendChild(layer1);
+appElm.appendChild(layer2);
+appElm.appendChild(layer3);
