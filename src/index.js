ee18edd48d23fbf56d73371dd53f069798b15d9a
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-01 12:03:11 +0100

    navigace zmizí po kliknutí na její položky

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 console.log('funguju!');
 const navBtn = document.querySelector('#nav-btn');
+const nav = document.querySelector('nav');
 navBtn.addEventListener('click', () => {
-  const nav = document.querySelector('nav');
   nav.classList.toggle('nav-closed');
+});
+const navItems = document.querySelectorAll('#nav-item');
+navItems.forEach((navItem) => {
+  navItem.addEventListener('click', () => {
+    nav.classList.add('nav-closed');
+  });
 });
