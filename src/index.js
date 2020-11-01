7bee4314da504a683411225ebaf6d42d8282fe2c
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-01 12:15:46 +0100

    objednávání

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 console.log('funguju!');
+
+//  Zprovoznění navigace
 const navBtn = document.querySelector('#nav-btn');
 const nav = document.querySelector('nav');
 navBtn.addEventListener('click', () => {
@@ -9,4 +11,17 @@ navItems.forEach((navItem) => {
   navItem.addEventListener('click', () => {
     nav.classList.add('nav-closed');
   });
+});
+
+//  Objednávání
+// Na tlačíko .order-btn pověste posluchač, který při kliknutí přidá na prvek .drink__cup třídu .drink__cup--selected. Zároveň změní nápis na tlačítku na Zrušit.
+const orderBtn = document.querySelector('.order-btn');
+const drinkCup = document.querySelector('.drink__cup');
+orderBtn.addEventListener('click', () => {
+  drinkCup.classList.toggle('drink__cup--selected');
+  if (drinkCup.classList.contains('drink__cup--selected')) {
+    orderBtn.textContent = 'Zrušit';
+  } else {
+    orderBtn.textContent = 'Objednat';
+  }
 });
