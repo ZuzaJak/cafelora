a949e5cdf3b4be1c29cd4a964e89168bd040caeb
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-01 11:44:00 +0100

    toggle nav-closed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 console.log('funguju!');
 const navBtn = document.querySelector('#nav-btn');
+navBtn.addEventListener('click', () => {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('nav-closed');
+});
